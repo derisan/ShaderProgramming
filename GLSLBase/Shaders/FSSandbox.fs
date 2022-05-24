4c07ab559ea1688cc4c863da99dee3f6f2116594
derisan <derisan@naver.com>
2022-05-24 09:46:56 +0900

    Lecture 4-1. 텍스쳐 좌표를 이용한 원 그리기

--- GLSLBase/Shaders/FSSandbox.fs
+++ GLSLBase/Shaders/FSSandbox.fs
@@ -1,8 +1,18 @@
 #version 450
 
-layout(location=0) out vec4 FragColor;
+in vec4 v_Color;
+
+out vec4 FragColor;
 
 void main()
 {
-	FragColor = vec4(1,1,1,1);
+	float dist = distance(v_Color.xy, vec2(0.5)); // ÅØ½ºÃÄ ÁÂÇ¥ÀÇ Áß¾Ó
+	vec4 newColor = vec4(0);
+
+	if(dist < 0.5)
+		newColor = vec4(1);
+	else
+		newColor = vec4(0);
+
+	FragColor = newColor;
 }
